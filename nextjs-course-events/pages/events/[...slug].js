14ba68e3a3c878df91ad9c1ca37a9b6a33741dae
Julian <jmeanchoff@trewknowledge.com>
2023-09-05 13:51:33 -0400

    Next.js course lectures 84-88

--- nextjs-course-events/pages/events/[...slug].js
+++ nextjs-course-events/pages/events/[...slug].js
@@ -1,7 +1,66 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert";
+
 function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const year = +filterData[0];
+  const month = +filterData[1];
+
+  if (
+    isNaN(year) ||
+    isNaN(month) ||
+    year > 2030 ||
+    year < 2021 ||
+    month < 1 ||
+    month > 12
+  ) {
+    return (
+      <>
+				<ErrorAlert>
+					<p>Invalid filter. Please adjust your values!</p>
+					<div className="center">
+						<Button link="/events">Show All Events</Button>
+					</div>
+				</ErrorAlert>
+      </>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: year,
+    month: month,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <>
+				<ErrorAlert>
+					<p>No events found for the chosen filter!</p>
+					<div className="center">
+						<Button link="/events">Show All Events</Button>
+					</div>
+				</ErrorAlert>
+      </>
+    );
+  }
+
+  const date = new Date(year, month - 1);
+
   return (
     <div>
-      <h1>Filtered Events</h1>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
     </div>
   );
 }
