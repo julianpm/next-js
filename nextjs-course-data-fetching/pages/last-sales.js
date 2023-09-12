211a775d802ad6f04b5cfd5c900587bcb1bd03bf
Julian <jmeanchoff@trewknowledge.com>
2023-09-12 15:38:26 -0400

    Next.js course lectures 112-115

---
+++ nextjs-course-data-fetching/pages/last-sales.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+function LastSalesPage() {
+  const [sales, setSales] = useState();
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    "https://nextjs-course-810e0-default-rtdb.europe-west1.firebasedatabase.app/sales.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
+
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  if (error) {
+    return <p>Failed to load.</p>;
+  }
+
+  if (!data || !sales) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+}
+export default LastSalesPage;
