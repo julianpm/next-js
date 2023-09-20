02b03dd859478408262a4f523d73c56b1c732c5b
Julian <jmeanchoff@trewknowledge.com>
2023-09-20 10:20:08 -0400

    Next.js course lectures 123-125

--- nextjs-course-events__dynamic/components/events/events-search.js
+++ nextjs-course-events__dynamic/components/events/events-search.js
@@ -13,7 +13,7 @@ function EventsSearch(props) {
     const selectedYear = yearInputRef.current.value;
     const selectedMonth = monthInputRef.current.value;
 
-    props.onSearch(selectedYear, selectedMonth);    
+    props.onSearch(selectedYear, selectedMonth);
   }
 
   return (
@@ -24,6 +24,7 @@ function EventsSearch(props) {
           <select id='year' ref={yearInputRef}>
             <option value='2021'>2021</option>
             <option value='2022'>2022</option>
+						<option value='2023'>2023</option>
           </select>
         </div>
         <div className={classes.control}>
--- nextjs-course-events__dynamic/pages/events/[eventId].js
+++ nextjs-course-events__dynamic/pages/events/[eventId].js
@@ -1,4 +1,4 @@
-import { getEventById, getAllEvents } from "../../helpers/api-util";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
@@ -9,9 +9,9 @@ function EventDetailPage(props) {
 
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+      <div className="center">
+        <p>Loading...</p>
+      </div>
     );
   }
 
@@ -39,17 +39,17 @@ export async function getStaticProps(context) {
     props: {
       selectedEvent: event,
     },
+		revalidate: 30
   };
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
-
+  const events = await getFeaturedEvents();
   const paths = events.map((event) => ({ params: { eventId: event.id } }));
 
   return {
     paths: paths,
-    fallback: false,
+    fallback: 'blocking',
   };
 }
 
