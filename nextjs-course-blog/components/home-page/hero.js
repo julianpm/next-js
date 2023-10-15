35fa4af701e779396d7dbb77f31378450d6f2bcb
Julian <jmeanchoff@trewknowledge.com>
2023-10-15 14:38:54 -0500

    Next.js course lectures 180-185

---
+++ nextjs-course-blog/components/home-page/hero.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+import classes from "./hero.module.css";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/moi.jpg"
+          alt="An image showing Julian scuba-diving"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Julian.</h1>
+      <p>I blog about ill shit.</p>
+    </section>
+  );
+}
+
+export default Hero;
