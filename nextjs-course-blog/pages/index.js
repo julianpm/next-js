8feb9547278a8fc4de3bd841ff03e9d630ce9a3b
Julian <jmeanchoff@trewknowledge.com>
2023-10-15 16:02:51 -0500

    Next.js course lectures 186-188

--- nextjs-course-blog/pages/index.js
+++ nextjs-course-blog/pages/index.js
@@ -1,10 +1,42 @@
-import Hero from '../components/home-page/hero';
+import FeaturedPosts from "../components/home-page/featured-posts";
+import Hero from "../components/home-page/hero";
+
+const DUMMY_POSTS = [
+  {
+    title: "Foo Bar",
+    slug: "foo-bar",
+    image: "scuba.jpeg",
+    excerpt: "Foo Bar is the foobariest thing ever.",
+		date: '2023-10-15'
+  },
+	{
+    title: "Foo Bar 2",
+    slug: "foo-bar-2",
+    image: "scuba.jpeg",
+    excerpt: "Foo Bar is the foobariest thing ever.",
+		date: '2023-10-15'
+  },
+	{
+    title: "Foo Bar 3",
+    slug: "foo-bar-3",
+    image: "scuba.jpeg",
+    excerpt: "Foo Bar is the foobariest thing ever.",
+		date: '2023-10-15'
+  },
+	{
+    title: "Foo Bar 4",
+    slug: "foo-bar-4",
+    image: "scuba.jpeg",
+    excerpt: "Foo Bar is the foobariest thing ever.",
+		date: '2023-10-15'
+  },
+];
 
 function HomePage() {
   return (
     <>
       <Hero />
-      {/* <FeaturedPosts /> */}
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </>
   );
 }
--- nextjs-course-blog/pages/posts/index.js
+++ nextjs-course-blog/pages/posts/index.js
@@ -1,5 +1,38 @@
-function AllPostsPage() {
+import AllPosts from '../../components/posts/all-posts';
 
+const DUMMY_POSTS = [
+  {
+    title: "Foo Bar",
+    slug: "foo-bar",
+    image: "scuba.jpeg",
+    excerpt: "Foo Bar is the foobariest thing ever.",
+		date: '2023-10-15'
+  },
+	{
+    title: "Foo Bar 2",
+    slug: "foo-bar-2",
+    image: "scuba.jpeg",
+    excerpt: "Foo Bar is the foobariest thing ever.",
+		date: '2023-10-15'
+  },
+	{
+    title: "Foo Bar 3",
+    slug: "foo-bar-3",
+    image: "scuba.jpeg",
+    excerpt: "Foo Bar is the foobariest thing ever.",
+		date: '2023-10-15'
+  },
+	{
+    title: "Foo Bar 4",
+    slug: "foo-bar-4",
+    image: "scuba.jpeg",
+    excerpt: "Foo Bar is the foobariest thing ever.",
+		date: '2023-10-15'
+  },
+];
+
+function AllPostsPage() {
+	return <AllPosts posts={DUMMY_POSTS} />
 }
 
 export default AllPostsPage;
