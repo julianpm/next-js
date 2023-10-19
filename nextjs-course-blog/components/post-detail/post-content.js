8927ecbb94a548d1b680dd56e8fdbb55493a4755
Julian <jmeanchoff@trewknowledge.com>
2023-10-19 16:11:18 -0500

    Next.js course lectures 196-198

--- nextjs-course-blog/components/post-detail/post-content.js
+++ nextjs-course-blog/components/post-detail/post-content.js
@@ -1,18 +1,68 @@
-import Markdown from 'react-markdown';
+import Markdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
-import React from 'react';
+import React from "react";
+import Image from "next/image";
 
 function PostContent(props) {
-	const { post } = props;
+  const { post } = props;
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            {
+              <Image
+                src={`/images/posts/${post.slug}/${image.properties.src}`}
+                alt={image.alt}
+                width={600}
+                height={300}
+              />
+            }
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => we need the 'js' part here
+      return (
+        <SyntaxHighlighter
+          language={language}
+          children={children}
+          style={atomDark}
+        />
+      );
+    },
+  };
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <Markdown>{post.content}</Markdown>
+      <Markdown components={customRenderers}>{post.content}</Markdown>
     </article>
   );
 }
