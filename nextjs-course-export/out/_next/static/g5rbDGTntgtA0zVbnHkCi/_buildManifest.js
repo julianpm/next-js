55d70ffe8541fb175cd6636e0db0ec2bda7fa42d
Julian <jmeanchoff@trewknowledge.com>
2023-10-24 21:33:45 -0400

    Next.js course lectures 215-218

---
+++ nextjs-course-export/out/_next/static/g5rbDGTntgtA0zVbnHkCi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-797e038b76cd75a94c8f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b6470206ebddc3eb4754.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
