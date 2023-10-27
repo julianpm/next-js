6951e99f06beafd32cde76985b70530b0887d838
Julian <jmeanchoff@trewknowledge.com>
2023-10-27 16:25:44 -0400

    Next.js course lectures 234-237

--- nextjs-course-authentication/components/profile/user-profile.js
+++ nextjs-course-authentication/components/profile/user-profile.js
@@ -1,6 +1,3 @@
-import { getSession } from "next-auth/client";
-import { useEffect, useState } from "react";
-
 import ProfileForm from "./profile-form";
 import classes from "./user-profile.module.css";
 
@@ -13,8 +10,8 @@ function UserProfile() {
   //     if (!session) {
   //       window.location.href = "/auth";
   //     } else {
-	// 			setIsLoading(false);
-	// 		}
+  // 			setIsLoading(false);
+  // 		}
   //   });
   // }, []);
 
@@ -22,10 +19,23 @@ function UserProfile() {
   //   return <p className={classes.profile}>Loading...</p>;
   // }
 
+  async function changePasswordHandler(passwordData) {
+    const response = await fetch("/api/user/change-password", {
+      method: "PATCH",
+      body: JSON.stringify(passwordData),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+    });
+
+		const data = await response.json();
+		console.log(data);
+  }
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 }
